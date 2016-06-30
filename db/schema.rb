41a7aa1a535af9464d1b97fbd219d2a215ac7fc4
Shibasis Patel <smartshibasish@gmail.com>
2016-06-29 22:44:13 -0700

    Renames User to Admin_User and Added many to many associations between Admin_User and Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626172634) do
+ActiveRecord::Schema.define(version: 20160629183959) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "first_name",      limit: 25
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160626172634) do
   end
 
   add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+
+  create_table "admin_users_pages", id: false, force: :cascade do |t|
+    t.integer "admin_user_id", limit: 4
+    t.integer "page_id",       limit: 4
+  end
+
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
 
   create_table "pages", force: :cascade do |t|
     t.integer  "subject_id", limit: 4
