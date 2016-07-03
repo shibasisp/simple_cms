ce85d14e956a4b0231c7d2fc5e1e852aaa101234
Shibasis Patel <smartshibasish@gmail.com>
2016-07-03 10:04:48 -0700

    Added CRUD in Subject, page, section

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,12 @@
-class Page < ActiveRecord::Base
-	belongs_to :subject
-	has_and_belongs_to_many :editors, :class_name => "AdminUser"
+class Section < ActiveRecord::Base
+
+  belongs_to :page
+  has_many :section_edits
+  has_many :editors, :through => :section_edits, :class_name => "AdminUser"
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("sections.position ASC") }
+  scope :newest_first, lambda { order("sections.created_at DESC")}
+
 end
